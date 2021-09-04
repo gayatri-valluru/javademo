f91ff4ed1623700f87ecb568c5027c40657d3f78
gayatri-valluru <74000019+gayatri-valluru@users.noreply.github.com>
2021-09-04 09:40:36 +0530

    Update sum.java

--- sum.java
+++ sum.java
@@ -1,8 +1,9 @@
-class Main {
-  public static void main(String[] args) {
-    int x = 5;
-    int y = 6;
-    int sum = x + y;
-    System.out.println(sum); 
-  }
-}
+public class Sum  
+{  
+public static void main(String args[])   
+{  
+int n1 = 22, n2 = 11, sum;  
+sum = n1 + n2;  
+System.out.println("The sum of numbers is: "+sum);  
+}  
+}  
